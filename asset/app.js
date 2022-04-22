3ba577e3e3e426c2e7c2a96e4417ec5c0bdb292a
IDRISSHACKER <idrisscoder@gmail.com>
2022-04-22 19:27:27 +0100

    Creation de la partie principale

--- asset/app.js
+++ asset/app.js
@@ -1,16 +1,27 @@
 window.onload = () => {
 
-    const progress = 10
-    const progressTimerState = 20
-    const circle = document.querySelector(".circle")
+    let circle = document.querySelector(".load")
+    let file = document.querySelector(".file")
 
-    let currentState = 0
+    const loading = ()=>{
+        let progress = 100
+        let progressTimerState = 20
+        let currentState = 0
+        const loaderTimer = setInterval(() => {
+            circle.style.background = `conic-gradient(#e85059 ${currentState}%, rgb(240, 197, 208, .5) 0%)`
+            if (currentState >= progress)
+                clearInterval(loaderTimer)
+            currentState++
+        }, progressTimerState)
+    }
 
-    const loaderTimer = setInterval(() => {
-        circle.style.background = `conic-gradient(rgb(224, 70, 109) ${currentState}%, rgb(240, 197, 208) 0%)`
-        circle.children[0].textContent = `${currentState}%`
-        if(currentState >= progress)
-            clearInterval(loaderTimer)
-        currentState++
-    }, progressTimerState)
+    circle.addEventListener("click", (e)=>{
+        loading()
+        e.preventDefault()
+        let outed = setTimeout(()=>{
+            clearTimeout(outed)
+            file.click()
+            return true
+        },1200)
+    })
 }
